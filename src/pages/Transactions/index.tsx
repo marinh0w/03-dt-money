fec8389d5f5d6e9917f56f8103d80bd5117d1aaf
marinh0w <gabrielgambardella14@gmail.com>
2023-05-02 11:54:23 -0300

    Fim tb transações

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,39 @@
+import { Summary } from "../../components/Summary";
+import { Header } from "../../components/header";
+import {
+  PriceHighlight,
+  TransactionContainer,
+  TransactionsTable,
+} from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">hamburguer</td>
+              <td>
+                <PriceHighlight variant="outcome">R$ -R$59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionContainer>
+    </div>
+  );
+}
